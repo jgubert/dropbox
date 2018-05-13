ebae7656ca687f8366f2410b473fd1752e217778
Unknown <cstefanischmidt@hotmail.com>
2018-05-13 15:51:13 -0300

    struct no servidor.h

--- include/dropboxClient.h
+++ include/dropboxClient.h
@@ -4,20 +4,6 @@
 #define MAXNAME 8
 #define MAXFILES 10
 
-struct file_info{
-  char name[MAXNAME];
-  char extension[MAXNAME];
-  char last_modified[MAXNAME];
-  int size;
-};
-
-struct client{
-  int devices[2];
-  char userid[MAXNAMES];
-  struct file_info info[MAXFILES];
-  int logged_int;
-};
-
 
 int login_server(char *host, int port);
 void sync_client();
